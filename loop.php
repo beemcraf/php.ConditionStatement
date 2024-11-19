9a8ea75c00397a965cb131a65bc19395c588f192
beemcraf <pomsanbeem9@gmail.com>
2024-11-19 11:07:14 +0700

    update

--- loop.php
+++ loop.php
@@ -11,13 +11,34 @@
         echo "<h2>While loop</h2>";
         echo "<br>";
         $x = 1;
-        while ($x<=5) {
+        while ($x <= 5) {
+            if ($x == 3) break; 
             echo "รอบที่ $x <br>";
             $x++;
         }
+        
         $i = 1;
-        while ($i<=40) {echo "-"; $i++;}
+        while ($i <= 40) {
+            echo "_"; 
+            $i++;
+        }
         echo "<br>";
+
+        echo "<h2>do..While loop</h2>";
+        echo "<br>";
+        $y = 1;
+        do {
+            echo "รอบที่ $y <br>";
+            $y++;
+            if ($y >= 5) break;  
+        } while ($y <= 99999999);  
+        echo "_______________________________________ <br>";
+
+        echo "<h2>For loop</h2>";
+        for ($i = 0; $i < 10; $i++) {
+            echo "For รอบที่ $i<br>";
+        }
+        echo "_______________________________________ <br>";
     ?>
 </body>
 </html>
