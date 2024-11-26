f78d2932f241b628e4385803a51cffa265846842
beemcraf <pomsanbeem9@gmail.com>
2024-11-26 10:26:38 +0700

    udate

---
+++ demo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ทดสอบส่งค่าโดยใช้ method GET</title>
+</head>
+<body>
+    <h2>ทดสอบส่งค่าจาก method GET (ผ่าน URL)</h2>
+    <a href="test_get_php?subject=PHP&web=www.udru.ac.th">คลิกได้เลยไม่ต้องแอ็ค</a>
+</body>
+</html>
--- test_get.php
+++ test_get.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ทดสอบส่งค่าโดยใช้ Method GET</title>
 </head>
 <body>
     
+        <h2>ทดสอบรับค่าจาก method GET (ผ่าน URL)</h2>
+        <?php
+        echo "ศึกษาเรื่อง:".$_GET['subject']. " ได้ที่ ". $_GET['web'];
+
+
+
+
+
+        ?>
 </body>
 </html>
