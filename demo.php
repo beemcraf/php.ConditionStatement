e450d9fc81156bcec084d4616cb164bf9516408c
beemcraf <pomsanbeem9@gmail.com>
2024-11-26 10:28:23 +0700

    update

--- demo.php
+++ demo.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ทดสอบส่งค่าโดยใช้ method GET</title>
+    <title>ทอสอบส่งค่าโดยใช้ Method GET</title>
 </head>
 <body>
-    <h2>ทดสอบส่งค่าจาก method GET (ผ่าน URL)</h2>
-    <a href="test_get_php?subject=PHP&web=www.udru.ac.th">คลิกได้เลยไม่ต้องแอ็ค</a>
+    <h2>ทดสอบส่งค่าเป็น method GET (ผ่าน URL)</h2>
+    <a href="test_get.php?subject=PHP&web=www.google.com">คลิ๊ก</a>
 </body>
 </html>
