ef1d291a3106e04a216703d3af5a4aa681cd4a41
beemcraf <pomsanbeem9@gmail.com>
2024-11-26 09:44:03 +0700

    update

--- globalVar.php
+++ globalVar.php
@@ -13,11 +13,31 @@
     echo "<br>";
     
     function myfunction(){
-        echo "อ่านจาก function ค่าของ x ==> ".$GLOBALS['x'];
+        // Using global to access the global variable
+        global $x;
+        $x = 35;
+        echo "อ่านจาก function ค่าของ x ==> ".$x;
     }
 
     myfunction();
+    echo "<br>";
+    echo "ค่าของ x ==> ".$x;
     echo "<hr>";
+
+    echo "<h2>การใช้ข้อมูลจาก server ด้วย \$_SERVER </h2>";
+    echo $_SERVER['PHP_SELF'];
+    echo "<br>";
+    echo $_SERVER['SERVER_NAME'];
+    echo "<br>";
+    echo $_SERVER['HTTP_HOST'];
+    echo "<br>";
+    //echo $_SERVER['HTTP_REFERER'];
+    echo "<br>";
+    echo $_SERVER['HTTP_USER_AGENT'];
+    echo "<br>";
+    echo $_SERVER['SCRIPT_NAME'];
+    echo "<br>";
+
     ?>
 </body>
 </html>
